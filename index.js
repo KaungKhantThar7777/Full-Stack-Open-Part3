f1c85866689911e52bc62bbe2de64e9d5878db17
kaung khant thar <kaungkhantthar@ucsm.edu.mm>
2020-10-20 14:42:37 +0630

    Solved 3.1: Phonebook backend step1

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+const persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "kkt",
+    number: "222",
+    id: 3,
+  },
+  {
+    name: "test",
+    number: "33",
+    id: 4,
+  },
+  {
+    name: "test2",
+    number: "3",
+    id: 5,
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+const port = 5000;
+app.listen(port, () => console.log(`App is listening on port ${port}`));
